9e0f9375e1af43829df31e692a221691f251bec7
chrismintan <chris_acs@hotmail.com>
2018-11-08 15:31:08 +0800

    deliverables

--- script.js
+++ script.js
@@ -1,7 +1,45 @@
 var monkey = "hello";
+console.log(tweets[0])
 ReactDOM.render(
     <div>
       {monkey}
     </div>,
     document.getElementById('root')
 );
+
+class User extends React.Component {
+  render() {
+    return (
+      <ul><span>{this.props.name}</span> Tweeted at: <span className="text-primary">{this.props.time}</span>
+        <li>{this.props.tweet}</li>
+      </ul>
+    )
+  }
+}
+
+class List extends React.Component {
+  render() {
+    let message = this.props.data.map( (data, index) => {
+      return <div className="card"><User key={index} name={data.user.name} tweet={data.text} time={data.created_at}></User></div>
+    })
+    return (
+      <div className="hov">{message}</div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <div>
+    <List data={tweets} />
+  </div>,
+  document.getElementById('root')
+)
+
+
+
+
+
+
+
+
+
